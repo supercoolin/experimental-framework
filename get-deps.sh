d1dd73eda9d668cce67bc38bd0a885edb6733273
Colin Evrard <130576490+supercoolin@users.noreply.github.com>
2023-08-08 21:41:10 +0200

    updTE

--- get-deps.sh
+++ get-deps.sh
@@ -1,1 +1,2 @@
-sudo apt install -y build-essential autotools automake zlib1g-dev libssl-dev pkg-config
+sudo apt install -y build-essential autotools automake zlib1g-dev \
+libssl-dev pkg-config meson cmake python3-pyelftools libnuma-dev liblua5.4-dev
