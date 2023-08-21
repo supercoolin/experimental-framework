5799e150781a10a8a36961854e09b42ccc42b626
supercoolin <colin.evrard@uclouvain.be>
2023-08-21 11:05:25 +0200

    fix paths problems

--- build-light.sh
+++ build-light.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export EXP_FRAMEWORK_PATH=$(dirname "$BASH_SOURCE")
+export START_DIR=$(pwd)
 cd dpdk
 source build_dpdk.sh
-source ../envs/env0.sh
-cd ../fastclick
+source $START_DIR/envs/env0.sh
+cd $START_DIR/fastclick
 source build_fastclick.sh
-cd ../pktgen
+cd $START_DIR/pktgen
 source build_pktgen.sh
