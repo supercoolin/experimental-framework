558e687382859cf121f0df8bc9c67393f9534837
Colin Evrard <130576490+supercoolin@users.noreply.github.com>
2023-08-24 11:10:00 +0200

    Add more dependencies for dpdk

--- get-deps.sh
+++ get-deps.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo apt install -y libpcap-dev build-essential autotools-dev automake zlib1g-dev \
- libssl-dev pkg-config meson cmake python3-pyelftools libnuma-dev liblua5.4-dev
+ libssl-dev pkg-config meson cmake python3-pyelftools libnuma-dev liblua5.4-dev \
+ libibverbs-dev libisal-dev libfdt-dev libbpf-dev
