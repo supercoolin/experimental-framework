03344e0e74e346373a830c65d21f37880d2d78fb
supercoolin <colin.evrard@uclouvain.be>
2023-09-05 10:00:04 +0200

    swap mstflint and llvm in build priorities

--- build-medium.sh
+++ build-medium.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export EXP_FRAMEWORK_PATH=$(pwd)
 export START_DIR=$(pwd)
-cd ofed-mstflint
-source build_mstflint.sh
+cd llvm
+source build_llvm.sh
 cd $START_DIR/dpdk
 source build_dpdk.sh
 source $START_DIR/envs/env0.sh
