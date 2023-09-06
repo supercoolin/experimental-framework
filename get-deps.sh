480321bd08e7c8e671151c7d0f0a2028880bb522
supercoolin <colin.evrard@uclouvain.be>
2023-09-06 14:01:16 +0200

    change deps to use downloaded llvm

--- get-deps.sh
+++ get-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt install -y libpcap-dev build-essential autotools-dev automake zlib1g-dev \
  libssl-dev pkg-config meson cmake python3-pyelftools libnuma-dev \
- libibverbs-dev libisal-dev libfdt-dev libbpf-dev llvm libpcap-dev liblua5.3-dev clang
+ libibverbs-dev libisal-dev libfdt-dev libbpf-dev llvm libpcap-dev liblua5.3-dev libncurses5
