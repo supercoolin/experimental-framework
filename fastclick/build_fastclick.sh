ecce43c97d06d84c84cde47e8e5a1cf38c5a5504
supercoolin <colin.evrard@uclouvain.be>
2023-09-07 18:28:38 +0200

    fix llvm env, change fastclick compilation

--- fastclick/build_fastclick.sh
+++ fastclick/build_fastclick.sh
@@ -12,6 +12,7 @@ export LD_LIBRARY_PATH=$EXP_FRAMEWORK_PATH/ofed-mstflint/mstflint-4.24.0/dist/li
 export CPATH=$EXP_FRAMEWORK_PATH/ofed-mstflint/mstflint-4.24.0/dist/include/mstflint:$CPATH
 export C_INCLUDE_PATH=$EXP_FRAMEWORK_PATH/ofed-mstflint/mstflint-4.24.0/dist/include/mstflint:$C_INCLUDE_PATH
 export CPLUS_INCLUDE_PATH=$EXP_FRAMEWORK_PATH/ofed-mstflint/mstflint-4.24.0/dist/include/mstflint:$CPLUS_INCLUDE_PATH
-CXX=clang++ ./configure --prefix=$PWD/$dest_dir --enable-dpdk --enable-intel-cpu --verbose --enable-select=poll CFLAGS="-O3 -march=native" CXXFLAGS="-std=c++11 -O3 -march=native"  --disable-dynamic-linking --enable-poll --enable-local --enable-flow --disable-task-stats --disable-cpu-load
+#CXX=clang++ ./configure --prefix=$PWD/$dest_dir --enable-dpdk --enable-intel-cpu --verbose --enable-select=poll CFLAGS="-O3 -march=native" CXXFLAGS="-std=c++11 -O3 -march=native"  --disable-dynamic-linking --enable-poll --enable-local --enable-flow --disable-task-stats --disable-cpu-load
+./configure --disable-linuxmodule --enable-userlevel --enable-user-multithread --enable-etherswitch --disable-dynamic-linking --enable-local --enable-dpdk --enable-research --disable-task-stats --enable-flow --enable-cpu-load --prefix $(pwd)/$dest_dir --enable-intel-cpu --enable-dpdk-pool --enable-rand-align CXX="clang++ -flto -fno-access-control" CC="clang -flto" CXXFLAGS="-std=gnu++14 -O3" LDFLAGS="-flto -fuse-ld=lld -Wl,-plugin-opt=save-temps" RANLIB="/bin/true" LD="ld.lld" READELF="llvm-readelf" AR="llvm-ar" --disable-bound-port-transfer --enable-dpdk-pool --enable-dpdk-packet --disable-dpdk-softqueue
 make -j 8 install
 echo $'To use packetmill optimally, add \nexport alias packetmill="export CXX=clang++ packetmill"\nto your bashrc'
