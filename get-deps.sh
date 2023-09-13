4b2b609ead988080679b6060d28d01c4d8af2615
supercoolin <colin.evrard@uclouvain.be>
2023-09-13 09:41:09 +0200

    remove clang from deps, remove clang dependency from the build command in fastclick

--- get-deps.sh
+++ get-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt install -y libpcap-dev build-essential autotools-dev automake zlib1g-dev \
  libssl-dev pkg-config meson cmake python3-pyelftools libnuma-dev libxml2-dev \
- libibverbs-dev libisal-dev libfdt-dev libbpf-dev llvm libpcap-dev liblua5.3-dev libncurses5
+ libibverbs-dev libisal-dev libfdt-dev libbpf-dev libpcap-dev liblua5.3-dev libncurses5
